9935222010386a383a298fdfc1b723f22ac68340
pvt-Tron <nickpvt@yahoo.it>
2026-02-04 09:33:09 +0100

    Update scan messages to remove SCANNING

--- scripts/scan.sh
+++ scripts/scan.sh
@@ -1,12 +1,12 @@
-printf "\n====== SCANNING FOR CREDIT CARD NUMBERS ======\n"
+printf "\n====== CREDIT CARD NUMBER REPORT ======\n"
 grep -rE --color=always '(\b[0-9]{4}[- ]?){3}[0-9]{4}\b' . --exclude-dir={.git} --line-number
 echo "========= CREDIT CARD SCAN COMPLETE =========="
 
-printf "\n==== SCANNING FOR SOCIAL SECURITY NUMBERS ====\n"
+printf "\n==== SOCIAL SECURITY NUMBER REPORT ====\n"
 grep -rE --color=always '\b[0-9]{3}-[0-9]{2}-[0-9]{4}\b' . --exclude-dir={.git} --line-number
 echo "======= SOCIAL SECURITY SCAN COMPLETE ========"
 
-printf "\n========= SCANNING FOR PHONE NUMBERS =========\n"
+printf "\n========= PHONE NUMBER REPORT =========\n"
 grep -rE --color=always '\b[0-9]{3}-[0-9]{3}-[0-9]{4}\b' . --exclude-dir={.git} --line-number
 grep -rE --color=always '\([0-9]{3}\) [0-9]{3}-[0-9]{4}' . --exclude-dir={.git} --line-number
 echo "========= PHONE NUMBER SCAN COMPLETE ========="
